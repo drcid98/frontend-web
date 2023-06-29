641e6fc4f84df0b37715e82197ec72b400ed929d
gvfigueroa <gvfigueroa@uc.cl>
2023-06-29 01:09:58 -0400

    waiting room

--- src/pages/Waiting.jsx
+++ src/pages/Waiting.jsx
@@ -1,14 +1,107 @@
-import React, { useEffect, useState , useContext} from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../auth/AuthContext';
-import Button from '../common/Button'
-import './Waiting.css'
-import Layout from '../common/Layout'
+import Button from '../common/Button';
+import './Waiting.css';
+import Layout from '../common/Layout';
 
-const UserCheck = () => { 
-  const { token } = useContext(AuthContext)
-  const [status, setStatus] = useState(null);
-  const [nombre, setNombre] = useState(null);
+
+
+const WaitingRoom = () => {
+  const { token } = useContext(AuthContext);
+  const [status, setStatus] = useState();
+  const [nombre, setNombre] = useState();
+  const [userID, setUserID] = useState();
+  const [available, setAvailable] = useState();
+  const [players, setPlayers] = useState(null);
+  const [clicked, setClicked] = useState(0);
+  const [first, setFirst] = useState();
+  const [print, setPrint] = useState();
+  const [color, setColor] = useState();
+  const [showButton, setShowButton] = useState(false);
+
+  const handleClick = () => {
+    if (!available){
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/games/available/diff`)
+      .then(response => {
+        setAvailable(response.data.id);
+        setClicked(prevClicked => prevClicked + 1);
+      })
+      .catch(error => {
+        // console.error(error);
+        createGame();
+      });
+    }
+    else {
+      axios.get(`${import.meta.env.VITE_BACKEND_URL}/games/${available}`)
+      .then(response => {
+        setAvailable(response.data.id);
+        setClicked(prevClicked => prevClicked + 1);
+      })
+      .catch(error => {
+        console.error(error);
+        //createGame();
+      });
+    }
+  };
+
+  const handleStart = () => {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/start/${available}`)
+      .then(response => {
+        if (response.data.game.num_players < 4){
+          setFirst(true);
+        }
+        else {
+          setPrint(`La partida está llena :( `);
+        }
+        
+        // setPlayers(`Hay ${response.data.game.num_players} jugadores acualmente, vuelva a intentarlo más tarde`);
+        console.log(response.data);
+        setColor(response.data.game.num_players);
+        if (players){
+          console.log("players")
+          setPlayers(response.data.game.num_players);
+        }
+    
+        // if (partida === 4) {
+        //   // Do something
+        // }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const getGame = () => {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/games/${available}`)
+      .then(response => {
+        setPlayers(response.data.num_players);
+        setColor(response.data.num_players);
+    
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const createGame = () => {
+    axios.post(`${import.meta.env.VITE_BACKEND_URL}/games`, {
+      turn: 1,
+      winner: null,
+      stage: 1,
+      num_players: 0
+    })
+      .then(response => {
+        setFirst(true);
+        setAvailable(response.data.game.id);
+    
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+
 
   useEffect(() => {
     axios({
@@ -19,37 +112,108 @@ const UserCheck = () => {
       }
     })
       .then(response => {
-        console.log(response.data.user)
+        console.log(response.data.user);
         setStatus(true);
         setNombre(response.data.username);
+        setUserID(response.data.userID)
       })
       .catch(error => {
         setStatus(false);
       });
   }, []);
 
+  useEffect(() => {
+    if (clicked){
+    if (available !== null && available !== undefined) {
+      console.log("starteando");
+      handleStart();
+    }
+   else {
+    console.log("creando");
+      createGame();
+    }
+  }
+  }, [clicked]);
 
-  if (!status){
+  useEffect(() => {
+    if (players !== null){
+      if (players < 4){
+      console.log("aca");
+      setPrint(`Hay ${players} jugadores acualmente, vuelva a intentarlo más tarde`);
+      }
+      else if (players === 4){
+        setShowButton(true);
+        setPrint(`Hay 4 jugadores, se puede empezar la partida!`);
+      }
+  }
+  }, [players]);
+
+
+
+  useEffect(() => {
+    if (first) {
+      const numeroAleatorio = Math.floor(Math.random() * (1000 - 0 + 1));
+      let color_final;
+      if (!color) {
+        color_final = 1;
+      } else {
+        color_final = color + 1;
+      }
+      console.log(color_final);
+      axios
+        .post(`${import.meta.env.VITE_BACKEND_URL}/players`, {
+          user_id: userID,
+          game_id: available,
+          name: `${nombre}`,
+          color: `${color_final}`,
+          troops: 24,
+          territories: 6,
+        })
+        .then(response => {
+          getGame();
+        })
+        .catch(error => {
+          if (error.response.status === 400){
+            setShowButton(true);
+            setPrint(`Ya te encuentras dentro de la partida`);
+          }
+          else {
+            setPrint(`La partida está llena :( `);
+          }
+          console.error(error);
+        });
+    }
+  }, [first]);
+  
+  
+
+ 
+
+  if (!status) {
     return (
-        <Layout>
+      <Layout>
         <div className="Waiting">
-            <h1>Debes registrarte o ingresar para poder jugar!</h1>
-            <Button path='/login' label='Login' />
-            <Button path='/signup' label='Signup' />
+          <h1>Debes registrarte o ingresar para poder jugar!</h1>
+          <Button path='/login' label='Login' />
+          <Button path='/signup' label='Signup' />
         </div>
-        </Layout>
+      </Layout>
     );
-    }
-    else {
-        return (
-            <Layout>
-            <div className="Waiting">
-                <h1>Bienvenid@, {nombre}</h1>
-                <h2>Te encuentras en la sala de espera</h2>
-            </div>
-            </Layout>
-        );
-    }
-}
+  } else {
+    return (
+      <Layout>
+        <div className="Waiting">
+          <h1>Bienvenid@, {nombre}</h1>
+          <h2>Te encuentras en la sala de espera</h2>
+          <button onClick={handleClick}>Checkear jugadores</button>
+          {showButton && (
+            <Button path='/game' label='Jugar' />
+          )}
+          <div>{print}</div>
+        </div>
+      </Layout>
+    );
+  }
+};
 
-export default UserCheck;
+export default WaitingRoom;
